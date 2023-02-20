795b8df0fe4e8932a00d77cf113d71632559ab18
rgnter <maros.prejsa1@gmail.com>
2023-02-20 22:39:36 +0100

    basic web

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Hatton', 'sans-serif']
+    },
+    colors: {
+      'primary': '#b4a8a0',
+      'secondary': '#2b2a29'
+    }
+  },
+  plugins: [],
+}
